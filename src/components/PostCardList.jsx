e0aafc23682497bab1ef8c72383f2b348a6a387b
alejandroirz <alejandroirz@outlook.com>
2020-04-22 01:14:57 -0600

    Api

--- src/components/PostCardList.jsx
+++ src/components/PostCardList.jsx
@@ -1,33 +1,36 @@
-import React, { Fragment } from 'react'
-import PostCard from './PostCard'
+import React, { Fragment } from "react";
+import PostCard from "./PostCard";
 
 function slicePostDescription(props) {
-  return props.posts.map(post => {
+  return props.posts.map((post) => {
     if (post.content.length > 50 || post.title.length > 30) {
-      return { ...post, title: post.title.substring(0, 30), img: post.img, description: (post.content.substring(0, 150) + "...") }
+      return {
+        ...post,
+        title: post.title.substring(0, 30),
+        img: post.img,
+        description: post.content.substring(0, 150) + "...",
+      };
     }
-    return { ...post, description: post.content }
-  })
+    return { ...post, description: post.content };
+  });
 }
 function PostCardList(props) {
-
   let postMocks = slicePostDescription(props);
   return (
     <Fragment>
-
-      {postMocks.map(post => {
+      {postMocks.map((post) => {
+        console.log(post.post_id);
 
         return (
-          < PostCard key={post.id}
-            postImage={post.img}
+          <PostCard
+            key={post.post_id}
             postTitle={post.title}
             postContent={post.description}
-            postId={post.id}
+            postId={post.post_id}
           />
-        )
-      }
-      )}
+        );
+      })}
     </Fragment>
-  )
+  );
 }
 export default PostCardList;
--- src/containers/Posts.jsx
+++ src/containers/Posts.jsx
@@ -1,18 +1,32 @@
-import React, { Fragment } from 'react';
-import PostCardList from '../components/PostCardList'
-import fakePosts from '../assets/mocks/fakePosts'
-function Posts() {
+import React, { Fragment, useEffect, useState } from "react";
+import PostCardList from "../components/PostCardList";
+import fakePosts from "../assets/mocks/fakePosts";
+import axios from "axios";
+const API = "http://localhost:3001/posts/0";
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    let ignore = false;
 
+    async function fetchData() {
+      const result = await axios(API);
+      if (!ignore) setPosts(result.data);
+    }
+
+    fetchData();
+    return () => {
+      ignore = true;
+    };
+  }, []);
   return (
     <Fragment>
       <div className="container">
         <div className="row">
-          <PostCardList
-            posts={fakePosts} className="col" />
+          <PostCardList posts={posts || []} className="col" />
         </div>
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
 export default Posts;
